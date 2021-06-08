8fc5fe751f2d85143a5493f2b97beb7c704501cb
Mars <jason.rubin1309@outlook.com>
2021-06-08 13:45:17 -0700

    minor fixes on NotFound Page

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-import { Box, Typography } from "@material-ui/core";
-
+import { Container, Box, Typography, Button } from "@material-ui/core";
 import { HOME_ROUTE } from "../../constants/routes";
+import imageUrl from "../../../src/assets/img/404-image.svg";
 
 export default function NotFound() {
   return (
-    <Box textAlign="center">
-      <Typography variant="h4" gutterBottom>Sorry, this page isn't available.</Typography>
-      <Typography variant="body1" color="textSecondary">
-        The link you followed may be broken, or the page may have been removed. <Link to={HOME_ROUTE}>Go back to home.</Link>
-      </Typography>
+    <Box display="flex" textAlign="center" height="100vh" alignItems="center" justifyContent="center">
+      <Container>
+        <img src={imageUrl} alt="" />
+        <Typography variant="h4" gutterBottom>
+          Sorry, this page isn't available.
+        </Typography>
+        <Typography variant="body1" color="textSecondary" gutterBottom>
+          The link you followed may be broken, or the page may have been removed.{" "}
+        </Typography>
+
+        <Button variant="contained" color="secondary" href={HOME_ROUTE}>
+          Go back to home
+        </Button>
+      </Container>
     </Box>
   );
 }
