5af53927ff103a6f863ca85e726939eceb753b19
Mars <jason.rubin1309@outlook.com>
2021-07-23 14:53:48 -0700

    Check for Https connection in SocketIO for Signal Server

--- client/src/hooks/useSocketIO.js
+++ client/src/hooks/useSocketIO.js
@@ -7,8 +7,9 @@ export default function useSocketIO(userName, options) {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
+    const isHttps = process.env.NODE_ENV === "production" 
     const newSocket = io.connect(
-      wrapHttps(process.env.REACT_APP_SS_DOMAIN),
+      wrapHttps(process.env.REACT_APP_SS_DOMAIN, isHttps),
       options);
 
     setSocket(newSocket);
